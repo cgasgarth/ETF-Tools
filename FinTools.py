5afd7a3746c8574db73f4cd0ae2444d76223ac89
cgasgarth <64235119+cgasgarth@users.noreply.github.com>
2022-05-02 12:57:35 -0400

    more small changes

--- FinTools.py
+++ FinTools.py
@@ -1,6 +1,6 @@
 from FinClasses import *
 
-def sortByWeight(stockList):
+def sortByWeight(stockList): #sort the stocklist by weighting of the stock
     # Traverse through 1 to len(arr)
     for i in range(1, len(stockList)):
         key = stockList[i]
@@ -13,16 +13,16 @@ def sortByWeight(stockList):
                 j -= 1
         stockList[j+1] = key
 
-def addStock(newStock, stockList, ETFWeight):
-    for stock in stockList:
+def addStock(newStock, stockList, ETFWeight): #add stock to stocklist, account if stock
+    for stock in stockList: #already exists
         if newStock.getTicker() == stock.getTicker():
             stock.setWeight(stock.getWeight() + newStock.getWeight() * ETFWeight)
             return
     newStock.setWeight(newStock.getWeight() * ETFWeight)
     stockList.append(newStock)
 
-def totalWeights(ETFList):
-    stockList = []
+def totalWeights(ETFList): #returns a list of stocks with the weighting of each being the 
+    stockList = [] #weighting in the entire portfolio
     for ETF in ETFList:
         ETFWeight = ETF.getWeight()
         for stock in ETF.getHoldings():
@@ -30,10 +30,9 @@ def totalWeights(ETFList):
     sortByWeight(stockList)
     return stockList
 
-def getNumHoldings(ETFList):
+def getNumHoldings(ETFList): 
     stockList = []
     for ETF in ETFList:
-        ETFWeight = ETF.getWeight()
         for stock in ETF.getHoldings():
-            addStock(stock, stockList, ETFWeight)
-    return len(stockList)
+            stockList.append(stock.getTicker())
+    return len(set(stockList)) #convert to set to make unqiue
---
+++ test.py
@@ -0,0 +1,2 @@
+test = ["test", "test", "test2"]
+print(len(set(test)))
